867edf79bbf7762aedfa3ab5ca0863e6a9512444
lintao <lintao.zhang@thoughtworks.com>
2020-10-16 17:56:29 +0800

    [Lintao] feat: practice 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,18 @@
-export const getPoetry
+export const getPoetry = () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => [data.origin, data.author, data.content])
+    .catch(console.log('fetch failed'))
+}
+
+export const getPoetryAsync = async () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  try {
+    const response  =  await fetch(url)
+    const data = await response.json();
+    return [data.origin, data.author, data.content]
+  } catch {
+    console.log('fetch failed')
+  }
+}
